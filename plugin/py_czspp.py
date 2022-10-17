e7d72b08636fffc41ef90f7d9b53a5c4b6ff9eb5
yungjr <65478511+yungjr@users.noreply.github.com>
2022-10-17 21:14:35 +0800

    Update py_czspp.py

--- plugin/py_czspp.py
+++ plugin/py_czspp.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append('..')
 from base.spider import Spider
 import base64
+from requests import session, utils
 from Crypto.Cipher import AES
 
 class Spider(Spider):  # 元类 默认的元类 type
@@ -13,6 +14,15 @@ class Spider(Spider):  # 元类 默认的元类 type
     def init(self, extend=""):
         print("============{0}============".format(extend))
         pass
+
+    def getCookie(self,url):
+        rsp = self.fetch(url,headers=self.header)
+        newurl = self.regStr(rsp.text, "btwaf=(\\S+)\";")
+        if len(newurl) > 0:
+            url = url + "?btwaf=" + newurl
+            rsp = self.fetch(url, headers=self.header)
+#            print("新地址：" + url)
+        return rsp.text
 
     def homeContent(self, filter):
         result = {}
@@ -36,14 +46,23 @@ class Spider(Spider):  # 元类 默认的元类 type
         return result
 
     def homeVideoContent(self):
-        rsp = self.fetch("https://czspp.com")
-        root = self.html(self.cleanText(rsp.text))
+        url = "https://czspp.com/"
+        hostText = self.getCookie(url)
+        root = self.html(self.cleanText(hostText))
         aList = root.xpath("//div[@class='mi_btcon']//ul/li")
         videos = []
         for a in aList:
             name = a.xpath('./a/img/@alt')[0]
             pic = a.xpath('./a/img/@data-original')[0]
             mark = a.xpath("./div[@class='hdinfo']/span/text()")[0]
+
+            try:
+                mark2 = a.xpath("./div[@class='rating']/text()")[0]
+                if len(mark2) > 0:
+                    mark = mark + ' ' + mark2
+            except IndexError:
+                pass
+
             sid = a.xpath("./a/@href")[0]
             sid = self.regStr(sid, "/movie/(\\S+).html")
             videos.append({
@@ -57,17 +76,29 @@ class Spider(Spider):  # 元类 默认的元类 type
         }
         return result
 
+    header = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"}
+#    cookies = ''
+
     def categoryContent(self, tid, pg, filter, extend):
         result = {}
-        url = 'https://czspp.com/{0}/page/{1}'.format(tid, pg)
-        rsp = self.fetch(url)
-        root = self.html(self.cleanText(rsp.text))
-        aList = root.xpath("//div[contains(@class,'mi_cont')]//ul/li")
+        url = 'https://czspp.com/{0}/page/{1}'.format(tid,pg)
+        hostText = self.getCookie(url)
+        root = self.html(self.cleanText(hostText))
+        aList = root.xpath("//div[contains(@class,'bt_img mi_ne_kd mrb')]/ul/li")
         videos = []
         for a in aList:
             name = a.xpath('./a/img/@alt')[0]
             pic = a.xpath('./a/img/@data-original')[0]
             mark = a.xpath("./div[@class='hdinfo']/span/text()")[0]
+
+            try:
+                mark2 = a.xpath("./div[@class='rating']/text()")[0]
+                if len(mark2) > 0:
+                    mark = mark + ' ' + mark2
+            except IndexError:
+                pass
+
             sid = a.xpath("./a/@href")[0]
             sid = self.regStr(sid, "/movie/(\\S+).html")
             videos.append({
@@ -86,8 +117,8 @@ class Spider(Spider):  # 元类 默认的元类 type
     def detailContent(self, array):
         tid = array[0]
         url = 'https://czspp.com/movie/{0}.html'.format(tid)
-        rsp = self.fetch(url)
-        root = self.html(self.cleanText(rsp.text))
+        hostText = self.getCookie(url)
+        root = self.html(self.cleanText(hostText))
         node = root.xpath("//div[@class='dyxingq']")[0]
         pic = node.xpath(".//div[@class='dyimg fl']/img/@src")[0]
         title = node.xpath('.//h1/text()')[0]
@@ -107,20 +138,14 @@ class Spider(Spider):  # 元类 默认的元类 type
         infoArray = node.xpath(".//ul[@class='moviedteail_list']/li")
         for info in infoArray:
             content = info.xpath('string(.)')
-            if content.startswith('类型'):
-                tpyen = ''
-                for inf in info:
-                    tn = inf.text
-                    tpyen = tpyen +'/'+'{0}'.format(tn)
-                    vod['type_name'] = tpyen.strip('/')
             if content.startswith('地区'):
                 tpyeare = ''
                 for inf in info:
                     tn = inf.text
                     tpyeare = tpyeare +'/'+'{0}'.format(tn)
                     vod['vod_area'] = tpyeare.strip('/')
-            if content.startswith('豆瓣'):
-                vod['vod_remarks'] = content
+            if content.startswith('年份'):
+                vod['vod_year'] = content.replace("年份：","")
             if content.startswith('主演'):
                 tpyeact = ''
                 for inf in info:
@@ -144,7 +169,7 @@ class Spider(Spider):  # 元类 默认的元类 type
             aList = vl.xpath('./a')
             for tA in aList:
                 href = tA.xpath('./@href')[0]
-                name = tA.xpath('./text()')[0]
+                name = tA.xpath('./text()')[0].replace('\xa0','')
                 tId = self.regStr(href, '/v_play/(\\S+).html')
                 vodItems.append(name + "$" + tId)
             joinStr = '#'
@@ -163,8 +188,8 @@ class Spider(Spider):  # 元类 默认的元类 type
 
     def searchContent(self, key, quick):
         url = 'https://czspp.com/xssearch?q={0}'.format(key)
-        rsp = self.fetch(url)
-        root = self.html(self.cleanText(rsp.text))
+        hostText = self.getCookie(url)
+        root = self.html(self.cleanText(hostText))
         vodList = root.xpath("//div[contains(@class,'mi_ne_kd')]/ul/li/a")
         videos = []
         for vod in vodList:
@@ -203,32 +228,40 @@ class Spider(Spider):  # 元类 默认的元类 type
         return msg[0:-paddingLen]
 
     def playerContent(self, flag, id, vipFlags):
+        result = {}
         url = 'https://czspp.com/v_play/{0}.html'.format(id)
         pat = '\\"([^\\"]+)\\";var [\\d\\w]+=function dncry.*md5.enc.Utf8.parse\\(\\"([\\d\\w]+)\\".*md5.enc.Utf8.parse\\(([\\d]+)\\)'
-        rsp = self.fetch(url)
-        html = rsp.text
+
+        hostText = self.getCookie(url)
+#        root = self.html(self.cleanText(hostText))
+        html = hostText
         content = self.regStr(html, pat)
         if content == '':
-            return {}
-        key = self.regStr(html, pat, 2)
-        iv = self.regStr(html, pat, 3)
-        decontent = self.parseCBC(base64.b64decode(content), key, iv).decode()
-        urlPat = 'video: \\{url: \\\"([^\\\"]+)\\\"'
-        vttPat = 'subtitle: \\{url:\\\"([^\\\"]+\\.vtt)\\\"'
-        str3 = self.regStr(decontent, urlPat)
-        str4 = self.regStr(decontent, vttPat)
-        self.loadVtt(str3)
+            str3 = url
+            pars = 1
+            header = self.header
+        else:
+            key = self.regStr(html, pat, 2)
+            iv = self.regStr(html, pat, 3)
+            decontent = self.parseCBC(base64.b64decode(content), key, iv).decode()
+            urlPat = 'video: \\{url: \\\"([^\\\"]+)\\\"'
+            vttPat = 'subtitle: \\{url:\\\"([^\\\"]+\\.vtt)\\\"'
+            str3 = self.regStr(decontent, urlPat)
+            str4 = self.regStr(decontent, vttPat)
+            self.loadVtt(str3)
+            pars = 0
+            header = ''
+            if len(str4) > 0:
+                result['subf'] = '/vtt/utf-8'
+                result['subt'] = ''
         result = {
-            'parse': '0',
+            'parse': pars,
             'playUrl': '',
             'url': str3,
-            'header': ''
+            'header': header
         }
-        if len(str4) > 0:
-            result['subf'] = '/vtt/utf-8'
-            # result['subt'] = Proxy.localProxyUrl() + "?do=czspp&url=" + URLEncoder.encode(str4)
-            result['subt'] = ''
         return result
+
 
     def loadVtt(self, url):
         pass
